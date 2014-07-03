26876c849d8591c9ce1e49a08cc5d2c7c66f1750
Emerson MX <emersonmx@gmail.com>
2014-07-03 11:32:28 -0300

    Adicionando licença aos códigos.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- core/src/com/gmail/emersonmx/tictactoe/model/GameApplication.java
+++ core/src/com/gmail/emersonmx/tictactoe/model/GameApplication.java
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) Emerson Max de Medeiros Silva
+
+  This file is part of tictactoe.
+
+  tictactoe is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  tictactoe is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with tictactoe.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.gmail.emersonmx.tictactoe.model;
 
 import com.badlogic.gdx.Gdx;
